12a893bbb5fdf8e0e2dedbb63caa93eab0698b2b
nourkarem <63581692+nourkarem@users.noreply.github.com>
2022-09-24 04:37:42 +0200

    Update environment.prod.ts

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-apiHost: 'https://udagram-api2-dev.us-east-1.elasticbeanstalk.com/api/v0' ,
+apiHost: 'http://udagram-api2-dev.us-east-1.elasticbeanstalk.com/api/v0' ,
 };
 
 /*
