062884e2ea2a714e4de6090fbaf80d0f9db79f29
nourkarem <63581692+nourkarem@users.noreply.github.com>
2022-09-24 04:19:41 +0200

    Update environment.ts

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'https://udagram-api2-dev.us-east-1.elasticbeanstalk.com /api/v0'
+  apiHost: 'https://udagram-api2-dev.us-east-1.elasticbeanstalk.com/api/v0/feed'
 };
 /*
  * For easier debugging in development mode, you can import the following file
